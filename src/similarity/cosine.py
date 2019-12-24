c6196462dffee0109c2557831a7670e93e848496
vathanathy <nt.vath@yahoo.com>
2019-12-24 17:29:08 +0100

    Column of similarity coefficient in df added

--- src/similarity/cosine.py
+++ src/similarity/cosine.py
@@ -1,9 +1,19 @@
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def similarity_cosine(id1, id2):
-	text_lst = []
-	text_lst.append(get_text(id1))
-	text_lst.append(get_text(id2))
+def similarity_cosine(graph, df):
+	sim_cosine = []
 
-	return cosine_similarity(X[0], X[1])
+	cv = CountVectorizer()
+	with open('../../data/training.txt', "r") as f:
+		for line in f:
+			line = line.split()
+			text_lst = []
+			text_lst.append(get_text(line[0]))
+			text_lst.append(get_text(line[1]))
+			X=cv.fit_transform(text_lst) #convert text into matrix of word count
+			sim_cosine.append(cosine_similarity(X[0], X[1]))
+	
+	df["Similarity-Cosine"] = sim_cosine
+	return
--- src/similarity/euclidean.py
+++ src/similarity/euclidean.py
@@ -2,11 +2,20 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import euclidean_distances
 
 
-def similarity_euclidean(id1, id2):
+def similarity_euclidean(graph, df):
+	sim_eu = []
+
 	cv = CountVectorizer()
-	text_lst = []
-	text_lst.append(get_text(id1))
-	text_lst.append(get_text(id2))
-	X=cv.fit_transform(text_lst) #convert text into matrix of word count
-	#Normalize the distance between 0 and 1 and substract by 1, so that the closer to 1 the stronger in similarity
-	return 1-euclidean_distances(X[0],X[1])/np.max((euclidean_distances(X)))
+	with open('../../data/training.txt', "r") as f:
+		for line in f:
+			line = line.split()
+			text_lst = []
+			text_lst.append(get_text(line[0]))
+			text_lst.append(get_text(line[1]))
+			X=cv.fit_transform(text_lst) #convert text into matrix of word count
+			#Normalize the distance between 0 and 1 and substract by 1, so that the closer to 1 
+			#the stronger in similarity
+			sim_eu.append(1-euclidean_distances(X[0],X[1])/np.max((euclidean_distances(X))))
+	
+	df["Similarity-Euclidean"] = sim_eu
+	return
